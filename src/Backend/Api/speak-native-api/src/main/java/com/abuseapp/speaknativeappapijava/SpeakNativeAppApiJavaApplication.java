583fba254c8230b4be43b0cd16d6fd7fefecdfc1
abuse-mac <az.bakhtybayev@gmail.com>
2020-10-20 01:00:17 +0200

    externalizing configs: moved to spec env folders

--- src/Backend/Api/speak-native-api/src/main/java/com/abuseapp/speaknativeappapijava/SpeakNativeAppApiJavaApplication.java
+++ src/Backend/Api/speak-native-api/src/main/java/com/abuseapp/speaknativeappapijava/SpeakNativeAppApiJavaApplication.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,11 +14,15 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import java.security.SecureRandom;
 
 @SpringBootApplication
-public class SpeakNativeAppApiJavaApplication {
+public class SpeakNativeAppApiJavaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpeakNativeAppApiJavaApplication.class, args);
 		System.out.println("Started");
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpeakNativeAppApiJavaApplication.class);
+	}
 }
