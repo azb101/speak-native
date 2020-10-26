00f4df6a68f255c3d77960731017d83f1573f111
abuse-mac <az.bakhtybayev@gmail.com>
2020-10-26 21:11:06 +0100

    moving to fat jar

--- src/Backend/Api/speak-native-api/src/main/java/com/abuseapp/speaknativeapi/SpeakNativeApiApplication.java
+++ src/Backend/Api/speak-native-api/src/main/java/com/abuseapp/speaknativeapi/SpeakNativeApiApplication.java
@@ -2,19 +2,12 @@ package com.abuseapp.speaknativeapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpeakNativeApiApplication extends SpringBootServletInitializer {
+public class SpeakNativeApiApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpeakNativeApiApplication.class, args);
 		System.out.println("Started");
-	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpeakNativeApiApplication.class);
 	}
 }
